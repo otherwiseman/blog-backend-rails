27111ae2f0cd25ea5a709d00fbfbbd697bdd92e7
ljmooney <ljmooney@ucdavis.edu>
2015-03-15 21:00:28 -0700

    Styling/Validations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,21 +4,25 @@ class PostsController < ApplicationController
 	end
 
 	def new 
+		@post = Post.new
 	end
 
 	def create
 		@post = Post.new(post_params)
-		@post.save
-
-		redirect_to @post
+		if @post.save
+			redirect_to @post
+		else 
+			render 'new'
+		end
 
 	end
 
 	def show 
 		@post = Post.find(params[:id])
 	end
+
 	private
-	def post_params
-		params.require(:post).permit(:title, :body)
+		def post_params
+			params.require(:post).permit(:title, :body)
+		end
 	end
-end
